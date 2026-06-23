b196ab5e43bf27e958ed7671da20b25cd0c3c5f1
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-06-23 00:03:14 -0800

    first schema

---
+++ part1/02_first_schema.sql
@@ -0,0 +1,11 @@
+-- db -> schema -> table -> rows
+
+-- if not exists, it is going to prevent an error if the schema is already created
+CREATE SCHEMA IF NOT EXISTS basics;
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- query
+SELECT schema_name
+FROM information_schema.schemata
+ORDER BY schema_name;
