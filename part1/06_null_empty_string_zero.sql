ebb5cff3b7c959074e3f2da312c6e4a877b071b0
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-06-25 07:25:57 -0800

    empty string, zero

---
+++ part1/06_null_empty_string_zero.sql
@@ -0,0 +1,33 @@
+
+-- null - unknown/missing value
+-- empty string - known string value, but it contains no character
+-- zero - actual numeric value of 0
+
+DROP TABLE IF EXISTS basics.value_examples;
+
+CREATE TABLE basics.value_examples (
+
+  id SERIAL PRIMARY KEY,
+
+  nickname TEXT,
+
+  bio TEXT,
+
+  score INTEGER
+);
+
+INSERT INTO basics.value_examples (nickname, bio, score)
+VALUES
+  -- nickname is null
+  (null, 'learning postgreSQL', 10),
+  ('', 'empty nickname', 20),
+  ('shina', '', 0),
+  ('sangam', null, null);
+
+-- SELECT * FROM basics.value_examples;
+
+SELECT * FROM basics.value_examples WHERE nickname IS NULL;
+
+-- write one query to find where nickname is empty string
+
+SELECT nickname FROM basics.value_examples WHERE nickname IS '';
