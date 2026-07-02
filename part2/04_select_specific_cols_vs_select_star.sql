d068556010272f705519a88f907656df1823947c
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-02 01:42:03 -0800

    select specific column

---
+++ part2/04_select_specific_cols_vs_select_star.sql
@@ -0,0 +1,17 @@
+
+
+-- select * - returns every cols
+-- select specific cols is going to return the cols that I want
+
+-- SELECT * FROM products;
+
+-- SELECT name, category, price, stock FROM products;
+
+-- AS creates an alias for the output of that column name
+-- makes the col name easier to read
+
+SELECT 
+  name AS product_name,
+  price AS selling_price,
+  stock AS available_quantity
+FROM products;
