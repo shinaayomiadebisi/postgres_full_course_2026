15cfb4f4a405429879a6948cb681198a26c9c65b
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-03 00:32:13 -0800

    like and ILike

---
+++ part2/07_like_ilike_patterns.sql
@@ -0,0 +1,21 @@
+-- like -> case sensitive pattern match
+-- ilike -> case insensitive pattern match
+-- % -> means any number of characters
+-- _ exactly one char
+
+
+-- % after wireless means anything can come after it
+SELECT name, price
+FROM products
+WHERE name LIKE 'Wireless%'; 
+
+-- Desk, desk, DESK
+SELECT name, category, price
+FROM products
+WHERE name ILIKE '%desk%'; 
+
+
+SELECT name, category, description
+FROM products
+WHERE name ILIKE '%chair%'
+  OR description ILIKE '&chair';
