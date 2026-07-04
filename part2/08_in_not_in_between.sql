989be0b86e3bd8c40cc7aa09242e3b77e6828eb4
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-04 15:53:46 -0800

    in not in between

---
+++ part2/08_in_not_in_between.sql
@@ -0,0 +1,22 @@
+
+-- IN -> value must match one item from the list
+-- NOT IN -> value must not match any item from the list
+-- BETWEEN -> value must be inside a range
+
+-- SELECT name, category, price
+-- FROM products
+-- WHERE category IN ('stationery', 'furniture');
+
+SELECT name, category, price
+FROM products
+WHERE category NOT IN ('electronics', 'furniture'); 
+
+SELECT name, price
+FROM products
+-- WHERE price >= 100 AND price <= 2000; 
+WHERE price BETWEEN 100 AND 2000; 
+
+SELECT name, category, price, stock
+FROM products
+WHERE category IN ('electronics', 'stationery')
+  AND price BETWEEN 10 AND 1499;
