a8b07fdd5e69f8f4e45152b65ce3c1d47eaec254
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-07 02:40:38 -0800

    delete rows

---
+++ part2/14_delete_rows.sql
@@ -0,0 +1,15 @@
+
+
+-- INSERT INTO products (name, category, price, stock, sku, description)
+-- VALUES ('temp product to be deleted', 'electronics', 345, 45, 'ELEC-TEMP-013', 'temp product desc');
+
+SELECT name, category, sku
+FROM products
+WHERE sku = 'ELEC-TEMP-013';
+
+DELETE FROM products
+WHERE sku = 'ELEC-TEMP-013';
+
+SELECT name, category, sku
+FROM products
+WHERE sku = 'ELEC-TEMP-013';
