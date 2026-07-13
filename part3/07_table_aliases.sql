19b09128bbb1b2318b74aa340bba36c63b1e7c87
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-13 10:50:34 -0800

    table aliases

---
+++ part3/07_table_aliases.sql
@@ -0,0 +1,21 @@
+
+
+-- aliases is going to make your queries shorter and easier to read
+
+-- posts.title
+-- I want to write like this p.title
+
+-- users.name instead, you want to do u.name
+
+SELECT
+  p.title AS post_title,
+  p.status,
+  p.views,
+  u.name AS author_name,
+  c.body AS comment_body
+FROM posts AS p
+INNER JOIN users AS u
+  ON p.user_id = u.id
+LEFT JOIN comments AS c
+  ON p.id = c.post_id
+ORDER BY p.views DESC;
