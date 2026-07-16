428a1691df05545926388fae3d276a1c26090385
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2026-07-16 03:47:42 -0800

    transactions

---
+++ part3/13_transactions.sql
@@ -0,0 +1,32 @@
+
+-- multiple sql statements run as one safe unit
+
+-- placing an order
+-- reduce stock of that product
+-- creating payment records
+-- transferring money
+-- creating user records with related profile data
+
+
+BEGIN;
+
+UPDATE posts
+SET status = 'published'
+WHERE title = 'Indexes for Beginners'
+  AND status = 'draft';
+
+UPDATE posts
+SET views = views + 50
+WHERE title = 'Indexes for Beginners';
+
+-- Something went wrong, or you changed your mind:
+-- ROLLBACK; 
+
+SELECT
+  title,
+  status,
+  views
+FROM posts
+WHERE title = 'Indexes for Beginners';
+
+COMMIT;
